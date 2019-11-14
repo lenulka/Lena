fa33c948acd684c30011018a760bc3beff73fa3a
lenulka <lenulka.bo4kova@ya.ru>
2019-11-14 23:49:06 +0500

    cool

--- js/lena.js
+++ js/lena.js
@@ -1,5 +1,11 @@
 const box = document.getElementsByClassName('map_wripper');
 const size = 10;
+const ships = {
+    '4': 1, 
+    '3': 2,
+    '2': 3,
+    '1': 4
+};
 [].forEach.call(box,function(item) {
   for (let i=1; i<size; i++) {
 		const row = document.createElement('div');
